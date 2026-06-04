f72071fbee4e5f9dfa389b4adb533dc9b334f32a
Pratuuu05 <avcharpratiksha@gmail.com>
2026-06-04 17:14:03 +0530

    Fix pdf parse version

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-const pdfParse = require("pdf-parse")
+const pdfParse = require("pdf-parse/lib/pdf-parse.js")
 const User = require("../models/User")
 const SavedJob = require("../models/SavedJob")
 const Job = require("../models/Job")
